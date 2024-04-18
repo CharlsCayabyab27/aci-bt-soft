b632649acbd312f9847632162d5ac9f6532c81c0
Kristian Marc Anthony Mendoza Ng <142019535+KMAN321@users.noreply.github.com>
2024-04-18 23:06:51 +0800

    pinalitan ko na 'yung "Para sa Soft Eng"

--- view_trans.php
+++ view_trans.php
@@ -21,7 +21,7 @@ Coded by www.creative-tim.com
     <link rel="icon" type="image/png" href="./assets/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>
-        Para sa Soft Eng
+        ACI-BT | Transactions
     </title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
         name='viewport' />
